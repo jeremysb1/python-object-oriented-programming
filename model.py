15047b3a16023ef3f1dad4945cf30037eba3be05
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-21 16:54:02 -0400

    added test method to Hyperparameter class

--- model.py
+++ model.py
@@ -52,3 +52,17 @@ class Hyperparameter:
         self.k = k
         self.data: TrainingData = training
         self.quality = float
+    
+    def test(self) -> None:
+        """Run the entire test suite."""
+        training_data: Optional["TrainingData"] = self.data()
+        if not training_data:
+            raise RuntimeError("Broken Weak Reference")
+        pass_count, fail_count = 0, 0
+        for sample in training_data.testing:
+            sample.classification = self.classify(sample)
+            if sample.matches():
+                pass_count += 1
+            else:
+                fail_count += 1
+        self.quality = pass_count / (pass_count + fail_count)
