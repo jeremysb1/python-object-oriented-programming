54fdf266f0cd9964f5a9f4e3161bf6c1001f6d1a
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-22 14:08:22 -0400

    added Distance class and ED subclass

--- model.py
+++ model.py
@@ -95,3 +95,18 @@ class TrainingData:
         classification = parameter.classify(sample)
         sample.classify(classification)
         return sample
+
+class Distance:
+    """Definition of a distance calculation."""
+
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        pass
+
+class ED(Distance):
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        return hypot(
+            s1.sepal_length - s2.sepal_length,
+            s1.sepal_width - s2.sepal_width,
+            s1.petal_length - s2.petal_length,
+            s1.petal_width - s2.petal_width
+        )
