1dbf4ae35a47e782546c975a9dd321f476ca3ed2
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-24 07:13:49 -0400

    added KnownSample class

--- model.py
+++ model.py
@@ -34,6 +34,51 @@ class Sample:
             f")"
         )
 
+class KnownSample(Sample):
+    """Abstract superclass for testing data, the species is set externally."""
+
+    def __init__(
+        self,
+        species: str,
+        sepal_length: float,
+        sepal_width: float,
+        petal_length: float,
+        petal_width: float,
+    ) -> None:
+        super().__init__(
+            sepal_length=sepal_length,
+            sepal_width=sepal_width,
+            petal_length=petal_length,
+            petal_width=petal_width,
+        )
+        self.species = species
+    
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"sepal_length={self.sepal_length}, "
+            f"sepal_width={self.sepal_width}, "
+            f"petal_length={self.petal_length}, "
+            f"petal_width={self.petal_width}, "
+            f"species={self.species!r}"
+            f")"
+        )
+    
+    @classmethod
+    def from_dict(cls, row: dict[str, str]) -> "KnownSample":
+        if row["species"] not in {"Iris-setosa", "Iris-versicolour", "Iris-virginica"}:
+            raise InvalidSampleError(f"invalid species in {row!r}")
+        try:
+            return cls(
+                species=row["species"],
+                sepal_length=float(row["sepal_length"]),
+                sepal_width=float(row["sepal_width"]),
+                petal_length=float(row["petal_length"]),
+                petal_width=float(row["petal_width"]),
+            )
+        except ValueError as ex:
+            raise InvalidSampleError(f"invalid {row!r}")
+
 class Hyperparameter:
     """A hyperparameter value and the overall quality of the classification."""
     def __init__(self, k: int, training: "TrainingData") -> None:
