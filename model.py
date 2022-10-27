ee2c8109218709e820310fcb2d042231361bbb14
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-27 07:07:16 -0400

    added SampleReader and BadSampleRow classes

--- model.py
+++ model.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 from asyncio import SubprocessTransport
 from collections.abc import Iterator
+import csv
 import datetime
+import enum
+from importlib.resources import path
+from pathlib import Path
 from typing import cast, Optional, Union, Iterable
 import weakref
 
@@ -196,6 +200,34 @@ class TrainingData:
         classification = parameter.classify(sample)
         sample.classify(classification)
         return sample
+
+class BadSampleRow(ValueError):
+    pass
+
+class SampleReader:
+    """See iris.names for attribute ordering in bezdekIris.data file"""
+
+    target_class = Sample
+    header = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+
+    def __init__(self, source: Path) -> None:
+        self.source = source
+    
+    def sample_iter(self) -> Iterator[Sample]:
+        target_class = self.target_class
+        with self.source.open() as source_file:
+            reader = csv.DictReader(source_file, self.header)
+            for row in reader:
+                try:
+                    sample = target_class(
+                        sepal_length=float(row["sepal_length"]),
+                        sepal_width=float(row["sepal_width"]),
+                        petal_length=float(row["petal_length"]),
+                        petal_width=float(row["petal_width"])
+                    )
+                except ValueError as ex:
+                    raise BadSampleRow(f"Invalid {row!r}") from ex
+                yield Sample
 
 class Distance:
     """Definition of a distance calculation."""
