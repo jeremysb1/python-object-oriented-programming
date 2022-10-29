d5d45c05e81f5a4a3df53e50b2eb86b1d597230e
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-29 11:21:48 -0400

    updated knownsample class

--- model.py
+++ model.py
@@ -55,40 +55,46 @@ class KnownSample(Sample):
         sepal_width: float,
         petal_length: float,
         petal_width: float,
+        purpose: int,
     ) -> None:
+        purpose_enum = Purpose(purpose)
+        if purpose_enum not in {Purpose.Training, Purpose.Testing}:
+            raise ValueError(f"Invalid: purpose: {purpose!r}: {purpose_enum}")
         super().__init__(
             sepal_length=sepal_length,
             sepal_width=sepal_width,
             petal_length=petal_length,
             petal_width=petal_width,
         )
+        self.purpose = purpose_enum
         self.species = species
+        self._classification: Optional[str] = None
+
+    def matches(self) -> bool:
+        return self.species == self.classification
+    
+    @property
+    def classification(self) -> Optional[str]:
+        if self.purpose == Purpose.Testing:
+            return self._classification
+        else:
+            raise AttributeError(f"Training samples have no classification.")
+    
+    @classification.setter
+    def classification(self, value: str) -> None:
+        if self.purpose == Purpose.Testing:
+            self._classification = value
+        else:
+            raise AttributeError(f"Training samples can't be classified.")
     
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
-            f")"
-        )
-    
-    @classmethod
-    def from_dict(cls, row: dict[str, str]) -> "KnownSample":
-        if row["species"] not in {"Iris-setosa", "Iris-versicolour", "Iris-virginica"}:
-            raise InvalidSampleError(f"invalid species in {row!r}")
-        try:
-            return cls(
-                species=row["species"],
-                sepal_length=float(row["sepal_length"]),
-                sepal_width=float(row["sepal_width"]),
-                petal_length=float(row["petal_length"]),
-                petal_width=float(row["petal_width"]),
-            )
-        except ValueError as ex:
-            raise InvalidSampleError(f"invalid {row!r}")
+        base_attributes = self.attr_dict
+        base_attributes["purpose"] = f"{self.purpose.value}"
+        base_attributes["species"] = f"{self.species!r}"
+        if self.purpose == Purpose.Testing and self._classification:
+            base_attributes["classification"] = f"{self._classification!r}"
+        attrs = ", ".join(f"{k}={v}" for k, v in base_attributes.items())
+        return f"{self.__class__.__name__}({attrs})"
 
 class TrainingKnownSample(KnownSample):
     """Training data."""
