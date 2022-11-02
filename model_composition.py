d08b82ebb203394407186760d5f4ad5d61ef468f
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-11-02 06:59:55 -0400

    added model_composition file with classes

---
+++ model_composition.py
@@ -0,0 +1,14 @@
+import collections
+from typing import Optional, Counter, NamedTuple
+import weakref
+import sys
+
+class Sample(NamedTuple):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+class KnownSample(NamedTuple):
+    sample: Sample
+    species: str
