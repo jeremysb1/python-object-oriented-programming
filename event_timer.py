26bd966cb0261eb5dd246eb0414a5c326ecd9c79
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-11-04 16:11:45 -0400

    added run method to class

--- event_timer.py
+++ event_timer.py
@@ -35,3 +35,36 @@ class Task:
             )
         else:
             return None
+
+class Scheduler:
+    """
+    Schedule tasks for execution.
+    Use :meth:`enter` to put tasks into the queue.
+    Use :meth:`run` to start processing.
+    Currently, this uses default time.sleep() which means ``after`` and ``delay`` are
+    in seconds.
+    """
+
+    def __init__(self) -> None:
+        self.tasks: List[Task] = []
+
+    def enter(
+        self,
+        after: int,
+        task: Callback,
+        delay: int = 0,
+        limit: int = 1,
+    ) -> None:
+        new_task = Task(after, task, delay, limit)
+        heapq.heappush(self.tasks, new_task)
+    
+    def run(self) -> None:
+        current_time = 0
+        while self.tasks:
+            next_task = heapq.heappop(self.tasks)
+            if (delay := next_task.scheduled - current_time) > 0:
+                time.sleep(next_task.scheduled - current_time)
+            current_time = next_task.scheduled
+            next_task.callback(current_time) # type: ignore [misc]
+            if again := next_task.repeat(current_time):
+                heapq.heappush(self.tasks, again)
