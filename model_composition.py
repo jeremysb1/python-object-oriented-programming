08a3d4c6f18b0dbc364e00bb3f8fb84911ee49b1
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-11-04 06:16:20 -0400

    added composite classes

--- model_composition.py
+++ model_composition.py
@@ -12,3 +12,17 @@ class Sample(NamedTuple):
 class KnownSample(NamedTuple):
     sample: Sample
     species: str
+
+class TestingKnownSample:
+    def __init__(self, sample: KnownSample, classification: Optional[str] = None) -> None:
+        self.sample = sample
+        self.classification = classification
+    
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(sample={self.sample!r}," 
+            f"classification={self.classification!r})"
+        )
+    
+class TrainingKnownSample(NamedTuple):
+    sample: KnownSample
