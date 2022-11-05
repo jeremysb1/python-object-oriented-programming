0032f459b8dbb4fd4e5ce25db15b2fdce15d3a34
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-11-05 14:24:32 -0400

    refactord classes

--- model_composition.py
+++ model_composition.py
@@ -1,5 +1,16 @@
-import collections
-from typing import Optional, Counter, NamedTuple
+from __future__ import annotations
+from typing import (
+    cast,
+    Callable,
+    Iterable,
+    Iterator,
+    List,
+    NamedTuple,
+    Optional,
+    Type,
+    Union,
+)
+from collections import defaultdict, Counter
 import weakref
 import sys
 
@@ -13,16 +24,33 @@ class KnownSample(NamedTuple):
     sample: Sample
     species: str
 
-class TestingKnownSample:
-    def __init__(self, sample: KnownSample, classification: Optional[str] = None) -> None:
-        self.sample = sample
-        self.classification = classification
-    
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}(sample={self.sample!r}," 
-            f"classification={self.classification!r})"
-        )
-    
+class TestingKnownSample(NamedTuple):
+    sample: KnownSample
+   
 class TrainingKnownSample(NamedTuple):
     sample: KnownSample
+
+def training_80(s: KnownSample, i: int) -> bool:
+    return i % 5 != 0
+
+def training_75(s: KnownSample, i: int) -> bool:
+    return i % 4 != 0
+
+def training_67(s: KnownSample, i: int) -> bool:
+    return i % 3 != 0
+
+TrainingList = List[TrainingKnownSample]
+TestingList = List[TestingKnownSample]
+
+def partition(
+    samples: Iterable[KnownSample],
+    rule: Callable[[KnownSample, int], bool]
+) -> tuple[TrainingList, TestingList]:
+    training_samples = [
+        TrainingKnownSample(s) for i, s in enumerate(samples) if rule(s, i)
+    ]
+    test_samples = [
+        TestingKnownSample(s) for i, s in enumerate(samples) if rule(s, i)
+    ]
+
+    return training_samples, test_samples
