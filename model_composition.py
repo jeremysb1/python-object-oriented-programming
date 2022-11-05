ef163326d346732ae04206637aad8641d6cc7400
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-11-05 16:07:44 -0400

    refactored partition function

--- model_composition.py
+++ model_composition.py
@@ -49,6 +49,34 @@ from typing import DefaultDict, Tuple, Iterator
 
 ModuloDict = DefaultDict[int, List[KnownSample]]
 
+def partition_2(
+    samples: Iterable[KnownSample], training_rule: Callable[[int], bool]
+) -> tuple[TrainingList, TestingList]:
+    
+    """Separate into non-equal buckets.
+    Combine buckets into testing and training subsets.
+    Chain into lists.
+    Highly dependent on Hash Seed randomization; use PYTHONHASHSEED=0 in the environment.
+    """
+
+    rule_multiple = 60
+    partitions: ModuloDict = collections.defaultdict(list)
+    for s in samples:
+        partitions[hash(s) % rule_multiple].append(s)
+    
+    training_partitions: list[Iterator[TrainingKnownSample]] = []
+    testing_partitions: list[Iterator[TestingKnownSample]] = []
+    for i, p in enumerate(partitions.values()):
+        if training_rule(i):
+            training_partitions.append(TrainingKnownSample(s) for s in p)
+        else:
+            testing_partitions.append(TestingKnownSample(s) for s in p)
+    
+    training = list(itertools.chain(*training_partitions))
+    testing = list(itertools.chain(*testing_partitions))
+    return training, testing
+
+
 Classifier = Callable[[int, DistanceFunc, TrainingList, AnySample], str]
 
 class Hyperparameter(NamedTuple):
